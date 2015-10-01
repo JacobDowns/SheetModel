8eab4274832463455f84b2bf0a713452764dccc8
JacobDowns <jacob.downs@umontana.edu>
2015-10-01 15:04:26 -0600

    stuff

--- sim_time_dependent_60_20.py
+++ sim_time_dependent_60_20.py
@@ -7,7 +7,7 @@ from dolfin import MPI, mpi_comm_world
 # Model input directory
 in_dir = "inputs_trough_60_20/"
 # Output directory
-out_dir = "out_trough_steady/"
+out_dir = "out_trough_time/"
 # Checkpoint directory
 check_dir = out_dir + "checkpoint/"
 # Process number
@@ -20,9 +20,9 @@ V_cg = FunctionSpace(mesh, "CG", 1)
 
 ### Model inputs
 
-# Initial sheet height
+# Load the steady state sheet height
 h_init = Function(V_cg)
-h_init.interpolate(Constant(0.05))
+File(in_dir + "h_steady.xml") >> h_init
 
 # Load the boundary facet function
 boundaries = FacetFunction('size_t', mesh)
@@ -31,6 +31,11 @@ File(in_dir + "boundaries.xml") >> boundaries
 # Load potential at 0 pressure
 phi_m = Function(V_cg)
 File(in_dir + "phi_m.xml") >> phi_m
+
+# Time dependent melt rate
+# Seconds per year
+spy = pcs['spy']
+m = Expression('(3.0 * (60000.0 - x[0]) / 60000.0) * (cos(((2.0 * pi) / spy) * t) + 1.0) / spy', pi = pi, spy = spy, t = 0.0)
 
 # Enforce 0 pressure bc at margin
 bc = DirichletBC(V_cg, phi_m, boundaries, 1)
@@ -43,7 +48,7 @@ prm['newton_solver']['relaxation_parameter'] = 1.0
 prm['newton_solver']['relative_tolerance'] = 1e-5
 prm['newton_solver']['absolute_tolerance'] = 1e-5
 prm['newton_solver']['error_on_nonconvergence'] = False
-prm['newton_solver']['maximum_iterations'] = 25
+prm['newton_solver']['maximum_iterations'] = 30
 
 model_inputs = {}
 model_inputs['mesh'] = mesh
@@ -51,6 +56,7 @@ model_inputs['h_init'] = h_init
 model_inputs['d_bcs'] = [bc]
 model_inputs['out_dir'] = out_dir
 model_inputs['newton_params'] = prm
+model_inputs['m'] = m
 model_inputs['constants'] = pcs
 model_inputs['opt_params'] = {'tol' : 1e-7, 'scale' : 10.0}
 
@@ -63,10 +69,10 @@ model = SheetModel(model_inputs, in_dir)
 # Seconds per day
 spd = pcs['spd']
 # End time
-T = 75.0 * spd
+T = 1.5 * spy
 # Time step
 dt = 60.0 * 60.0 * 8.0
-# Irataion count
+# Iteration count
 i = 0
 
 while model.t < T:
@@ -77,9 +83,9 @@ while model.t < T:
   model.step(dt)
   
   if i % 1 == 0:
-    model.write_pvds()
+    model.write_pvds(set(["pfo", "phi", "m", "h"]))
     
-  if i % 1 == 0:
+  if i % 3 == 0:
     model.write_xmls()
   
   if MPI_rank == 0: 
