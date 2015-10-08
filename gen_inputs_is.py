78884992b6952a41fae067537b1cbe7ff90f9b39
JacobDowns <jacob.downs@umontana.edu>
2015-10-08 11:15:52 -0600

    better melt field

--- gen_inputs_is.py
+++ gen_inputs_is.py
@@ -22,6 +22,35 @@ File(in_dir + "B.xml") >> B
 H = Function(V_cg)
 File(in_dir + "H.xml") >> H
 
+
+### Create a melt rate pattern from accumulation
+
+adot = Function(V_cg)
+File(in_dir + "adot.xml") >> adot
+
+# Get rid of badness near the margin
+xs = project(Expression("x[0]"), V_cg)
+adot = project(conditional(gt(xs, -4.825e5), -adot, 2.8), V_cg)
+
+# Smooth out the melt field with Poisson equation
+def m_boundary(x, on_boundary):
+  return on_boundary
+  
+bc = DirichletBC(V_cg, adot, m_boundary)
+
+m = TrialFunction(V_cg)
+v = TestFunction(V_cg)
+a = inner(grad(m), grad(v)) * dx
+f = Function(V_cg)
+L = f * v * dx
+
+m = Function(V_cg)
+solve(a == L, m, bc)
+
+# Convert to m/s rather than m/a
+m = project(m / pcs['spy'], V_cg)
+plot(m, interactive = True)
+
 # Constants
 rho_i = pcs['rho_i']
 rho_w = pcs['rho_w']
@@ -40,3 +69,5 @@ File(out_dir + "p_i.xml") << p_i
 File(out_dir + "p_i.pvd") << p_i
 File(out_dir + "phi_0.xml") << phi_0
 File(out_dir + "phi_0.pvd") << phi_0
+File(out_dir + "m.xml") << m
+File(out_dir + "m.pvd") << m
