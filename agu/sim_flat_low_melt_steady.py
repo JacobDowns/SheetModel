4a05c4d6a61d3ece257b53959a9af222b9e5047d
JacobDowns <jacob.downs@umontana.edu>
2015-11-23 18:00:22 -0700

    low melt sim on flat bed

---
+++ agu/sim_flat_low_melt_steady.py
@@ -0,0 +1,88 @@
+"""
+Default steady state for a flat bed with melt cut in half.
+"""
+
+import sys
+from dolfin import *
+from dolfin_adjoint import *
+from dolfin import MPI, mpi_comm_world
+sys.path.insert(0, '../')
+from sheet_model import *
+from constants import *
+
+# Model input directory
+in_dir = "inputs_flat_bed/"
+# Output directory
+out_dir = "out_flat_bed_low_melt_steady/"
+# Checkpoint directory
+check_dir = out_dir + "checkpoint/"
+# Process number
+MPI_rank = MPI.rank(mpi_comm_world())
+# Load mesh and create function spaces
+mesh = Mesh(in_dir + "mesh_60_20.xml")
+V_cg = FunctionSpace(mesh, "CG", 1)
+
+
+### Model inputs
+
+# Initial sheet height
+h_init = Function(V_cg)
+h_init.interpolate(Constant(0.05))
+
+# Moulin melt function
+m_moulin = Function(V_cg)
+File(in_dir + "m_moulin.xml") >> m_moulin
+m_moulin = project(0.5 * m_moulin, V_cg)
+
+pcs['k'] = 5e-3
+
+# Newton solver params
+prm = NonlinearVariationalSolver.default_parameters()
+prm['newton_solver']['relaxation_parameter'] = 1.0
+prm['newton_solver']['relative_tolerance'] = 1e-5
+prm['newton_solver']['absolute_tolerance'] = 1e-5
+prm['newton_solver']['error_on_nonconvergence'] = False
+prm['newton_solver']['maximum_iterations'] = 25
+
+model_inputs = {}
+model_inputs['mesh'] = mesh
+model_inputs['h_init'] = h_init
+model_inputs['out_dir'] = out_dir
+model_inputs['newton_params'] = prm
+model_inputs['m'] = m_moulin
+model_inputs['constants'] = pcs
+
+# Create the sheet model
+model = SheetModel(model_inputs, in_dir)
+
+
+### Run the simulation
+
+# Seconds per day
+spd = pcs['spd']
+# End time
+T = 90.0 * spd
+# Time step
+dt = 60.0 * 60.0 * 12.0
+# Iteration count
+i = 0
+
+while model.t < T:  
+  
+  if MPI_rank == 0: 
+    current_time = model.t / spd
+    print ('%sCurrent time: %s %s' % (fg(1), current_time, attr(0)))
+  
+  model.step(dt)
+  
+  if i % 2 == 0:
+    model.write_pvds(['h', 'u_b', 'm', 'pfo', 'k'])
+    
+  if i % 2 == 0:
+    model.write_xmls(['h', 'pfo', 'phi'])
+  
+  if MPI_rank == 0: 
+    print
+    
+  i += 1
+
