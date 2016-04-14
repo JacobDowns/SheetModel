3be06411c2a476fdd6478f5ad16c59796a57f8bd
Jacob Downs <jacob.downs@umontana.edu>
2016-04-14 10:37:31 -0600

    parallel test

--- parallel_test.py
+++ parallel_test.py
@@ -22,6 +22,14 @@ scale_functions = ScaleFunctions(input_file, k_min, k_max, u_b_max = 80.0)
 
 ### Setup the model
 
+prm = NonlinearVariationalSolver.default_parameters()
+prm['newton_solver']['relaxation_parameter'] = 1.0
+prm['newton_solver']['relative_tolerance'] = 1e-12
+prm['newton_solver']['absolute_tolerance'] = 1e-12
+prm['newton_solver']['error_on_nonconvergence'] = False
+prm['newton_solver']['maximum_iterations'] = 50
+
+
 # Seconds per day
 spd = pcs['spd']
 model_inputs = {}
@@ -29,6 +37,7 @@ pcs['k_c'] = 1e-1
 model_inputs['constants'] = pcs
 model_inputs['input_file'] = input_file
 model_inputs['out_dir'] = 'parallel_test'
+model_inputs['newton_params'] = prm
 model = ChannelModel(model_inputs)
 
 # Update the melt
@@ -45,4 +54,4 @@ model.set_u_b(scale_functions.get_u_b(model.t))
 dt = 60.0 * 60.0 * 8.0
 model.step(dt)
 
-File("parallel_test/phid.pvd") << model.phi
+File("parallel_test/phie.pvd") << model.phi
