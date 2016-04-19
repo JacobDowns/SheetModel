f8e4cc99e6219460a7d659cf87a497bb1fa1ca00
JacobDowns <jacob.downs@umontana.edu>
2016-04-19 13:34:43 -0600

    stuff

--- sim_ref_winter.py
+++ sim_ref_winter.py
@@ -13,7 +13,7 @@ from scale_functions import *
 MPI_rank = MPI.rank(mpi_comm_world())
 
 # Scale functions for determining winter sliding speed
-input_file = 'inputs_sheet/steady/ref_steady1.hdf5'
+input_file = 'inputs_sheet/steady/ref_steady.hdf5'
 scale_functions = ScaleFunctions(input_file, 5e-3, 5e-3)
 
 prm = NonlinearVariationalSolver.default_parameters()
--- sim_ref_winter_sliding_only.py
+++ sim_ref_winter_sliding_only.py
@@ -14,7 +14,7 @@ from scale_functions import *
 MPI_rank = MPI.rank(mpi_comm_world())
 
 # Scale functions for determining winter sliding speed
-input_file = 'inputs_sheet/steady/ref_steady1.hdf5'
+input_file = 'inputs_sheet/steady/ref_steady.hdf5'
 scale_functions = ScaleFunctions(input_file, 5e-3, 5e-3, u_b_max = 80.0)
 
 prm = NonlinearVariationalSolver.default_parameters()
