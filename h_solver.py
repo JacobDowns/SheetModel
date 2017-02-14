8f94a08c1fb85430e2ce9ba2f2455c7626eff4f2
Jacob Downs <jacob.downs@umontana.edu>
2017-02-14 15:12:26 -0700

    petsc ts

--- h_solver.py
+++ h_solver.py
@@ -1,6 +1,6 @@
 from dolfin import *
-from scipy.integrate import ode
 from dolfin import MPI, mpi_comm_world
+from petsc4py import PETSc
 
 """Solves ODE for the sheet height h with phi fixed."""
 
@@ -14,64 +14,80 @@ class HSolver():
     ### Get a few fields and parameters from the model
     
     # Effective pressure
-    N = model.N   
+    self.N = model.N   
     # Sliding speed
-    u_b = model.u_b
-    # Initial model time
-    t0 = model.t
+    self.u_b = model.u_b
     # Rate factor
-    A = model.pcs['A']
+    self.A = model.pcs['A']
     # Distance between bumps
-    l_r = model.pcs['l_r']
+    self.l_r = model.pcs['l_r']
     # Bump height
     h_r = model.h_r
-    # Initial sheet height
-    h0 = model.h.vector().array()
     # Bump height vector
     h_r_n = h_r.vector().array()
+    # Shet height function
+    self.h = model.h
     
 
     ### Set up the sheet height ODE
+
+    # h as petsc4py vec (metaphysically linked to h)
+    self.h_v = as_backend_type(self.h.vector()).vec()
+    comm = model.mesh.mpi_comm().tompi4py() 
+
+
+    # Object that encapsulates ODE object for PETSc
+    class ODE(object):
       
-    # Right hand side for the gap height ODE
-    def rhs(t, h_n):
-      # Ensure that the sheet height is positive
-      h_n[h_n < 0.0] = 0.0
-      # Sheet opening term
-      w_n = u_b.vector().array() * (h_r_n - h_n) / l_r
-      # Ensure that the opening term is non-negative
-      w_n[w_n < 0.0] = 0.0
-      # Sheet closure term
-      v_n = A * h_n * N.vector().array()**3
-      # Return the time rate of change of the sheet
-      dhdt = w_n - v_n
-      return dhdt
-    
-    # Set up ODE solver
-    ode_solver = ode(rhs).set_integrator('dopri5', max_step = 60.0, atol = 1e-8, rtol = 1e-12, nsteps = 1000)
-    ode_solver.set_initial_value(h0, t0)
+      def __init__(self):
+        #  Part of  opening term that doesn't depend on h
+        self.v_o_0 = None
+        # Part of closure term that doesn't depend on h
+        self.v_c_0 = None
+      
+      def rhs(self, ts, t, h, dhdt):
+        h_n = h.getArray()
+        # Sheet opening term
+        v_o_n = self.v_o_0 * (h_r_n - h_n) 
+        # Ensure that the opening term is non-negative
+        v_o_n[v_o_n < 0.0] = 0.0
+        # Sheet closure term
+        v_c_n = self.v_c_0 * h_n
+        # Set right hand side
+        dhdt.setArray(v_o_n - v_c_n) 
+
+    # Create PETSc time stepping solver  
+    ode = ODE()
+    ode_solver = PETSc.TS().create(comm=comm)
+    ode_solver.setType(ode_solver.Type.RK)
+    ode_solver.setRHSFunction(ode.rhs)
+    ode_solver.setTime(0.0)
+    ode_solver.setInitialTimeStep(0.0, 1.0)
+    ode_solver.setTolerances(atol=1e-8, rtol=1e-12)
+    ode_solver.setMaxSteps(50000)
+    ode_solver.setExactFinalTime(ode_solver.ExactFinalTimeOption.MATCHSTEP)
 
 
     ### Set local variables    
     
+    self.ode = ode
     self.ode_solver = ode_solver
     self.model = model
     
 
   # Step the gap height h forward by dt
   def step(self, dt):
-    # Use the current solution for h as the initial condition for the ODE solver
-    # for this step. Normally, we could just use the solution from the previous
-    # time step, but this is important if we want to reset the model (say manually
-    # assign a different h)
-  
-    self.ode_solver.y[:] = self.model.h.vector().array()
-      
-    # Step h 
-    self.ode_solver.integrate(self.model.t + dt)
-
-    # Retrieve values from the ODE solver    
-    self.model.h.vector().set_local(self.ode_solver.y)
+    # Precompute parts of the rhs that don't depend on h, but do depend on other
+    # time dependent fields
+    self.ode.v_o_0 = self.u_b.vector().array() / self.l_r
+    self.ode.v_c_0 = self.A * self.N.vector().array()**3
+    # Step the ODE forward
+    self.ode_solver.setTime(0.0)
+    self.ode_solver.setDuration(dt)
+    self.ode_solver.solve(self.h_v)
+    print('steps %d (%d rejected)'
+          % (self.ode_solver.getStepNumber(), self.ode_solver.getStepRejections()))
+    # Apply changes to vector
     self.model.h.vector().apply("insert")
   
  
